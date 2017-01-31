7ba33d4272de3d2fc5fd65fefd79aa5e7c5c06b7
root <root@ip-172-31-37-183.us-west-2.compute.internal>
2017-01-31 19:53:29 +0000

    parsing arguments from slash command

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var wyd = express();
 
-wyd.get('/', function(req, res){
+var bodyParser = require('body-parser')
+
+wyd.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+wyd.post('/', function(req, res){
+  console.log(req.body.text)
   res.send("hello world")
 });
 
 
-wyd.listen(9000, function () {
+wyd.listen(80, function () {
   console.log('wyd listening on port 9000!')
 })
