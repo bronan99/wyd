8e377bfb1fc7ddf90384f8df6ab6c5e847f4ef06
Brendan Ronan <bronan@Brendans-MacBook-Pro.local>
2017-02-08 15:14:21 -0500

    mongo service & dummy test

---
+++ test/mongoServiceTest.js
@@ -0,0 +1,10 @@
+var expect    = require("chai").expect;
+
+describe("Color Code Converter", function() {
+  describe("dummy test", function() {
+    it("passes a test", function() {
+        expect(true).to.equal(true);
+    })
+    
+  })
+});
