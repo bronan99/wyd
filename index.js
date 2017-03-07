c44b87731bd7a990b26f9fa86ef3725fdd26fbe5
EC2 Default User <ec2-user@ip-172-31-37-183.us-west-2.compute.internal>
2017-03-07 18:31:36 +0000

    confirm button and endoint

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ var bodyParser = require('body-parser')
 var pollTemplate = {
   "response_type": "in_channel",
   "text": "",
+  "replace_original": true,
   "attachments": [
-    {"text": ":thumbsup:  :question:  :thumbsdown:"}
+    { "callback_id": "confirm",
+      "actions": [
+        {
+          "name": "confirm",
+          "text": "confirm",
+          "type": "button",
+          "value": "confirm",
+	  "style": "primary"
+        }
+      ]
+    },
+    { "text": ":thumbsup:  :question:  :thumbsdown:" }
   ]
 }
+
 
 wyd.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -19,12 +32,21 @@ wyd.post('/', function(req, res){
   var args = req.body.text.split(" ");
   console.log(args);
 
-  pollTemplate.text = args[3] + "\n" + args[0] + " at " + args[1] + "\n" + args[2] + "\n" 
+  pollTemplate.text = "*what:* " + args[3] + "\n*when:* " + args[0] + " at " + args[1] + "\n*where:* " + args[2] + "\n" 
 
   res.send(pollTemplate)
 });
+
+wyd.post("/confirm", function(req, res) {
+  var message = JSON.parse(req.body.payload);
+  console.log(message.original_message);
+ 	
+  res.send("ok");
+})
 
 
 wyd.listen(80, function () {
   console.log('wyd listening on port 9000!')
 })
+
+
