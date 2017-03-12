7b0b33b7941839872598d209c063f79f36c000c5
Brendan Ronan <bronan@Brendans-MacBook-Pro.local>
2017-03-12 15:50:47 -0400

    confirm message implemented

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict'
 var express = require('express');
 var wyd = express();
 
@@ -8,14 +9,15 @@ var pollTemplate = {
   "text": "",
   "replace_original": true,
   "attachments": [
-    { "callback_id": "confirm",
+    {
+      "callback_id": "confirm",
       "actions": [
         {
           "name": "confirm",
           "text": "confirm",
           "type": "button",
           "value": "confirm",
-	  "style": "primary"
+          "style": "primary"
         }
       ]
     },
@@ -23,30 +25,47 @@ var pollTemplate = {
   ]
 }
 
+var confirmTemplate = {
+  "response_type": "in_channel",
+  "text": "",
+  "replace_original": true,
+}
 
-wyd.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+
+wyd.use(bodyParser.urlencoded({     
   extended: true
 }));
 
-wyd.post('/', function(req, res){
+wyd.post('/', function (req, res) {
   var args = req.body.text.split(" ");
   console.log(args);
 
-  pollTemplate.text = "*what:* " + args[3] + "\n*when:* " + args[0] + " at " + args[1] + "\n*where:* " + args[2] + "\n" 
+  pollTemplate.text = "*what:* " + args[3] + "\n*when:* " + args[0] + " at " + args[1] + "\n*where:* " + args[2] + "\n"
 
   res.send(pollTemplate)
 });
 
-wyd.post("/confirm", function(req, res) {
-  var message = JSON.parse(req.body.payload);
-  console.log(message.original_message);
- 	
-  res.send("ok");
-})
+wyd.post("/confirm", function (req, res) {
+  //var message = JSON.parse(req.body.payload);
 
+  confirmTemplate.text = generateConfirmMessage(req.body.payload);
+  res.send(confirmTemplate);
+})
 
 wyd.listen(80, function () {
   console.log('wyd listening on port 9000!')
 })
 
+var generateConfirmMessage = function(msg) {
+  var team = message.team.domain;
+  var text = message.original_message.text.replace(/\r?\n/g, ":").split(":");
+
+  var eventName = text[1].trim();
+  var location = text[5].trim();
+  var datetime = text[3].trim().split(" ");
+  var day = datetime[0];
+  var time = datetime[2];
+
+  return "*Confirmed:* *" + eventName + "*\n" + day + " at " + time + "\n" + location; 
+}
 
