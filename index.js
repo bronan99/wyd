1b81dd49e88fce0fa43986c5a34c96e0eeb66a3b
EC2 Default User <ec2-user@ip-172-31-37-183.us-west-2.compute.internal>
2017-03-12 22:00:30 +0000

    barebones working version

--- index.js
+++ index.js
@@ -28,7 +28,13 @@ var pollTemplate = {
 var confirmTemplate = {
   "response_type": "in_channel",
   "text": "",
-  "replace_original": true,
+  "attachments": [
+    {
+      "text": "",
+      "color": "#7CD197"
+    }
+  ],
+  "replace_original": false
 }
 
 
@@ -38,7 +44,7 @@ wyd.use(bodyParser.urlencoded({
 
 wyd.post('/', function (req, res) {
   var args = req.body.text.split(" ");
-  console.log(args);
+  //console.log(args);
 
   pollTemplate.text = "*what:* " + args[3] + "\n*when:* " + args[0] + " at " + args[1] + "\n*where:* " + args[2] + "\n"
 
@@ -46,9 +52,11 @@ wyd.post('/', function (req, res) {
 });
 
 wyd.post("/confirm", function (req, res) {
-  //var message = JSON.parse(req.body.payload);
+  var message = JSON.parse(req.body.payload);
+  var response = generateConfirmMessage(message);
 
-  confirmTemplate.text = generateConfirmMessage(req.body.payload);
+  confirmTemplate.text = response[0];
+  confirmTemplate.attachments[0].text = response[1];
   res.send(confirmTemplate);
 })
 
@@ -56,16 +64,22 @@ wyd.listen(80, function () {
   console.log('wyd listening on port 9000!')
 })
 
-var generateConfirmMessage = function(msg) {
+var generateConfirmMessage = function(message) {
   var team = message.team.domain;
-  var text = message.original_message.text.replace(/\r?\n/g, ":").split(":");
+  var text = message.original_message.text.replace(/\r?\n/g, ":").replace(/[*]/g,  "").split(":");
 
   var eventName = text[1].trim();
   var location = text[5].trim();
-  var datetime = text[3].trim().split(" ");
+  var datetime = text[3].trim()
   var day = datetime[0];
   var time = datetime[2];
 
-  return "*Confirmed:* *" + eventName + "*\n" + day + " at " + time + "\n" + location; 
+
+  console.log(eventName);
+  console.log(location);
+  console.log(datetime);
+
+  return ["*Confirmed:* " + eventName,  "\n" + datetime + "\n" + location]; 
 }
+
 
